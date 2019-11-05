efad2458142be3532d7ff487f7d1196374847a08
treem0 <travismolter@gmail.com>
2019-11-04 20:09:37 -0800

    fixed my import, didnt fix it FAK

--- pokedex/index.js
+++ pokedex/index.js
@@ -1,5 +1,5 @@
-import PokemonApp from '../pokedex/PokedexApp.js';
+import PokedexApp from '../pokedex/PokedexApp.js';
 
-const app = new PokemonApp();
+const app = new PokedexApp();
 const dom = app.renderDOM();
 document.body.prepend(dom);
