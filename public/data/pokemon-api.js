14d61009be929f0941eb68d4a8a374df71964f6b
treem0 <travismolter@gmail.com>
2019-11-09 13:00:08 -0800

    working on search and paging, work?

--- public/data/pokemon-api.js
+++ public/data/pokemon-api.js
@@ -1,16 +1,16 @@
-const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
+const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex?';
 
-export async function getPokemon() {  
+export async function getPokemon() {
     let queryString = window.location.hash.slice(1);
-    const url = `${URL}?${queryString}`;
+    const url = `${URL}${queryString}`;
 
     const response = await fetch(url);
     const data = await response.json();
-   
-    if (data.Response === 'False') {
+
+    if (data.response === 'false'){
         return {
             Search: [],
-            count: 0
+            totalResults: 0
         };
     }
 
--- public/pokedex/Paging.js
+++ public/pokedex/Paging.js
@@ -1,7 +1,6 @@
 import Component from '../Component.js';
 
 class Paging extends Component {
-
     onRender(dom) {
         const prevButton = dom.querySelector('.prev');
         const nextButton = dom.querySelector('.next');
@@ -15,11 +14,11 @@ class Paging extends Component {
         function updateControls() {
             const queryString = window.location.hash.slice(1);
             const searchParams = new URLSearchParams(queryString);
-
             const parsedPage = parseInt(searchParams.get('page'));
             if (isNaN(parsedPage)) {
                 page = 1;
-            } else {
+            } 
+            else {
                 page = parsedPage;
             }
         }
@@ -34,7 +33,6 @@ class Paging extends Component {
             const queryString = window.location.hash.slice(1);
             const searchParams = new URLSearchParams(queryString);
             searchParams.set('page', page + increment);
-
             window.location.hash = searchParams.toString();
         }
 
@@ -48,11 +46,10 @@ class Paging extends Component {
     }
 
     renderHTML() {
-        const perPage = 20;
-        const count = this.props.count;
+        const totalResults = this.props.totalResults;
         const queryString = window.location.hash.slice(1);
         const searchParams = new URLSearchParams(queryString);
-
+        
         let page = 1;
         const parsedPage = parseInt(searchParams.get('page'));
         if (isNaN(parsedPage)) {
@@ -60,14 +57,17 @@ class Paging extends Component {
         } else {
             page = parsedPage;
         }
-
-        if (!count) {
+        
+        if (!totalResults) {
             return /*html*/`
-                <p class="paging">No results, try another search</p>
+            <p class="paging">No results, try another search</p>
             `;
         }
-
-        const lastPage = Math.ceil(count / perPage);
+        
+        const perPage = 20;
+        const lastPage = Math.ceil(totalResults / perPage);
+        const start = (page * perPage) - (perPage -1, totalResults);
+        const end = Math.min(start + perPage - 1, totalResults);
 
         return /*html*/`
             <p class="paging-section">
