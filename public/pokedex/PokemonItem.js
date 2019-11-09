9c4b9ca4eac5fa588dc7c8a6e3c3e464eada1545
treem0 <travismolter@gmail.com>
2019-11-09 11:33:02 -0800

    working on search and paging, work?

--- public/pokedex/PokemonItem.js
+++ public/pokedex/PokemonItem.js
@@ -3,6 +3,7 @@ import Component from '../Component.js';
 class PokemonItem extends Component {
     renderHTML() {
         const pokemon = this.props.pokemon;
+        
 
         return /*html*/`
         <li class="single-pokemon-card">
