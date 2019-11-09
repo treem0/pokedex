c423af8597a5221b55ebb492b5dc7e8ec18e7404
treem0 <travismolter@gmail.com>
2019-11-09 12:04:53 -0800

    working on search and paging, work?

--- public/data/pokemon-api.js
+++ public/data/pokemon-api.js
@@ -1,18 +1,16 @@
-
 const URL = 'https://alchemy-pokedex.herokuapp.com/api/pokedex';
 
-export async function getPokemon() {
+export async function getPokemon() {  
     let queryString = window.location.hash.slice(1);
-    
     const url = `${URL}${queryString}`;
 
     const response = await fetch(url);
     const data = await response.json();
-
+   
     if (data.Response === 'False') {
         return {
             Search: [],
-            totalResults: 0
+            count: 0
         };
     }
 
